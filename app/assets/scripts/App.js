39e427eb26ca4f70344af9b5e2db1b30ee1198cc
Alejandro Baldwin <alejandro.baldwin98@gmail.com>
2019-10-08 18:51:53 -0400

    Added a contact modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
 //import RevealOnScroll from './modules/RevealOnScroll';
-//import $ from 'jquery';
+import $ from 'jquery';
 //import _ from 'lodash';
 //import StickyHeader from './modules/StickyHeader';
-//import Modal from './modules/Modal';
+import Modal from './modules/Modal';
 
 //new RevealOnScroll($("item-to-reveal"), "%from-bottom");
 //const StickyHeader = new StickyHeader();
-//const Modal = new Modal();
+const modal = new Modal;
 
 const mobileMenu = new MobileMenu;
